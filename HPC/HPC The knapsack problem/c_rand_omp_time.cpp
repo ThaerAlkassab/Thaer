5d9fd246fac4f47bbb784a498e091e1cfb52df3d
ThaerAlkassab <68050705+ThaerAlkassab@users.noreply.github.com>
2023-11-25 13:49:34 +0100

    The knapsack problem
    problem solve in c

---
+++ HPC/HPC The knapsack problem/c_rand_omp_time.cpp
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+int w[10], p[10], v[10][10], n, i, j, cap, x[10] = {0};
+
+int max(int i, int j) {
+    return ((i > j) ? i : j);
+}
+
+int knap(int i, int j) {
+    int value;
+
+    #pragma omp parallel shared(v, w, p) private(value)
+    {
+        #pragma omp for
+        for (int k = 1; k <= n; k++) {
+            if (v[k][j] < 0) {
+                if (j < w[k])
+                    value = knap(k - 1, j);
+                else
+                    value = max(knap(k - 1, j), p[k] + knap(k - 1, j - w[k]));
+                #pragma omp critical
+                {
+                    v[k][j] = value;
+                }
+            }
+        }
+    }
+
+    return (v[i][j]);
+}
+
+int main() {
+    clock_t start, end;
+    double cpu_time_used;
+
+    printf("\nEnter the number of elements\n");
+    scanf("%d", &n);
+
+    // Generate random profits and weights
+    for (i = 1; i <= n; i++) {
+        p[i] = rand() % 100; // Adjust the range as needed
+        w[i] = rand() % 50;  // Adjust the range as needed
+    }
+
+    printf("\nEnter the capacity \n");
+    scanf("%d", &cap);
+
+    for (i = 0; i <= n; i++)
+        for (j = 0; j <= cap; j++)
+            if ((i == 0) || (j == 0))
+                v[i][j] = 0;
+            else
+                v[i][j] = -1;
+
+    start = clock(); // Start measuring time
+
+    int profit, count = 0;
+
+    profit = knap(n, cap);
+    i = n;
+    j = cap;
+
+    while (j != 0 && i != 0) {
+        if (v[i][j] != v[i - 1][j]) {
+            x[i] = 1;
+            j = j - w[i];
+            i--;
+        } else
+            i--;
+    }
+
+    end = clock(); // Stop measuring time
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("Items included are\n");
+    printf("Sl.no\tweight\tprofit\n");
+    for (i = 1; i <= n; i++)
+        if (x[i])
+            printf("%d\t%d\t%d\n", ++count, w[i], p[i]);
+
+    printf("Total profit = %d\n", profit);
+    printf("Time taken: %f seconds\n", cpu_time_used);
+
+    return 0;
+}
+
